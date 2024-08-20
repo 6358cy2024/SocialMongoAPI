701c4eb0f16aca2465f0bbcbb83bd6b9f6735e5b
Chris Yanez <cy2019@hotmail.com>
2024-08-20 09:34:11 -0400

    updated

--- config/connection.js
+++ config/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://127.0.0.1:27017/twittleDB')
+mongoose.connect('mongodb://127.0.0.1:27017/SocialMango')
 
 module.exports = mongoose.connection;
