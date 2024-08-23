0465c1f57b5eb21e2450cc0253d54933d41c7c1d
Chris Yanez <cy2019@hotmail.com>
2024-08-23 17:40:06 -0400

    most of thought controller done

--- controllers/thought_controller.js
+++ controllers/thought_controller.js
@@ -4,9 +4,8 @@ module.exports = {
 
     async createThought(req, res) {
         const user = await User.findById(req.params.user_id);
-        const newThought = await Thought.create({
+        const newThought = await Thought.create({//Thought Body
             thoughtText: req.body.thoughtText,
-            createdAt: req.body.createdAt,
             username: user.username
         })
         user.thoughts.push(newThought._id);
@@ -18,14 +17,14 @@ module.exports = {
         })
     },
     async getAllThoughts(req, res) {
-        const thoughts = await Thought.find().populate({
+        const thoughts = await Thought.find().populate({//don't include id and username
             path: 'username',
-            select: 'email -_is -username'
+            select: 'email -_id -username'
         });
         res.json(thoughts)
     },
     async getSingleThought(req, res) {
-        const thought = await Thought.findById(req.params.thought_id);
+        const thought = await Thought.findById(req.params.thought_id);//simple findById
 
         res.json(thought);
     },
@@ -34,22 +33,35 @@ module.exports = {
         try {
             const updatedThought = await Thought.findByIdAndUpdate(req.params.thought_id, req.body, { new: true });
             if (!updatedThought) {
-                return res.status(404).json({ message: 'Thought not found' });
+                return res.status(404).json({ message: 'Thought not found' });//error message
             }
             res.json({
-                message: 'Thought Updated',
+                message: 'Changes Made',
                 thought: updatedThought
             });
         } catch (error) {
-            res.status(500).json({ message: 'Error updating thought', error: error.message });
+            res.status(500).json({ message: 'Unable to make changes', error: error.message });
         }
     },
     async deleteThought(req, res) {
-        const thought = await Thought.findById(req.params.thought_id);
+        const thought = await Thought.findById(req.params.thought_id);//same find by, just for a different purpose
         await thought.deleteOne();
 
         res.json({
             message: 'Thought Removed'
         })
-    }
+    },
+
+    //Reaction section
+
+    async createReaction(req, res) {
+        const thought = await Thought.findById(req.params.thought_id);
+
+        thought.reactions.push(req.body);//push the reaction to the thought
+        await thought.save();
+        res.json(thought);//then call the new thought including the reaction now
+    },
+
+    //async deleteReaction(req, res)
+
 }
